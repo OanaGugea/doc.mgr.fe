8cfe5c168e25c199e924bd45bc53fc7b602d26fa
Oana Gugea <oana.gugea@corandus.com>
2020-08-25 19:28:33 +0300

    styling added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './navbar/navbar.component';
-import {MatToolbarModule } from '@angular/material/toolbar';
-import {MatButtonModule } from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
-import { RouterModule } from '@angular/router';
-import { MesajComponent } from './mesaj/mesaj.component';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatListModule} from '@angular/material/list';
-
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { RouterModule } from "@angular/router";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatListModule } from "@angular/material/list";
+import { VanzariComponent } from "./components/vanzari/vanzari.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    MesajComponent
-  ],
+  declarations: [AppComponent, NavbarComponent, VanzariComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -30,13 +25,8 @@ import {MatListModule} from '@angular/material/list';
     MatInputModule,
     MatTabsModule,
     MatListModule,
-    
-    
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-
-export class AppModule {
-  
- }
+export class AppModule {}
